f3b458ef4ca21301dece9fe4f2413b1ef8615426
anudeeps352 <anudeeps352@gmail.com>
2025-03-11 04:23:45 +0530

    typo fix

--- src/lib/validation/aboutSchema.ts
+++ src/lib/validation/aboutSchema.ts
@@ -5,7 +5,7 @@ export const Schema = z.object({
   lastName: z.string().min(1, 'Last Name must be at least 1 characters'),
   location: z.string().min(2, 'Location must be at least 2 characters'),
   title: z.string().min(2, 'Title must be at least 2 characters'),
-  company: z.string().min(2, 'Compamy must be at least 2 characters'),
+  company: z.string().min(2, 'Company must be at least 2 characters'),
 });
 
 export type FormFields = z.infer<typeof Schema>;
